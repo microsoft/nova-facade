a7f5d9adf0b810b61992d185d93f58ef677b3522
Stanislaw Wilczynski <Stanislaw.Wilczynski@microsoft.com>
2023-03-27 21:26:53 +0200

    add test using nova test utils directly

--- packages/examples/src/Feedback/FeedbackContainer.test.tsx
+++ packages/examples/src/Feedback/FeedbackContainer.test.tsx
@@ -1,8 +1,16 @@
 import { composeStories } from "@storybook/react";
 import * as stories from "./FeedbackContainer.stories";
-import { render, screen } from "@testing-library/react";
+import { FeedbackContainer } from "./FeedbackContainer";
+import { act, render, screen } from "@testing-library/react";
 import * as React from "react";
 import "@testing-library/jest-dom";
+import {
+  createMockEnvironment,
+  getOperationName,
+  MockPayloadGenerator,
+  NovaMockEnvironmentProvider,
+} from "@nova/react-test-utils";
+import { getSchema } from "../testing-utils/getSchema";
 
 const { Primary, Liked } = composeStories(stories);
 
@@ -15,6 +23,36 @@ describe("FeedbackContainer", () => {
 
   it("should show unlike button", async () => {
     render(<Liked />);
+    const button = await screen.findByRole("button", { name: "Unlike" });
+    expect(button).toBeInTheDocument();
+  });
+
+  it("should show unlike when used directly", async () => {
+    const environment = createMockEnvironment(getSchema());
+    render(<FeedbackContainer />, {
+      wrapper: ({ children }) => {
+        return (
+          <NovaMockEnvironmentProvider environment={environment}>
+            {children}
+          </NovaMockEnvironmentProvider>
+        );
+      },
+    });
+    await act(async () =>
+      environment.graphql.mock.resolveMostRecentOperation((operation) => {
+        console.log(getOperationName(operation))
+        return MockPayloadGenerator.generate(operation, {
+          Feedback: () => ({
+            id: "1",
+            doesViewerLike: true,
+            message: {
+              text: "Some text",
+            },
+          }),
+        });
+      }),
+    );
+
     const button = await screen.findByRole("button", { name: "Unlike" });
     expect(button).toBeInTheDocument();
   });
