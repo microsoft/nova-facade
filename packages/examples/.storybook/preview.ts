04233651acf328373b439524bfce16d99a1e2ecf
Stanislaw Wilczynski <Stanislaw.Wilczynski@microsoft.com>
2023-05-13 10:42:50 +0200

    simplify preview

--- packages/examples/.storybook/preview.ts
+++ packages/examples/.storybook/preview.ts
@@ -1,15 +1,5 @@
 import type { Preview } from "@storybook/react";
 
-const preview: Preview = {
-  parameters: {
-    actions: { argTypesRegex: "^on[A-Z].*" },
-    controls: {
-      matchers: {
-        color: /(background|color)$/i,
-        date: /Date$/,
-      },
-    },
-  },
-};
+const preview: Preview = {};
 
 export default preview;
