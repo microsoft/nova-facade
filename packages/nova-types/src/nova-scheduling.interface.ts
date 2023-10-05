86cd7d58bc923708d7d9e15577ef1536e2c2e1a7
Drew Atkinson <dratkins@microsoft.com>
2023-10-05 16:38:13 -0700

    comments

--- packages/nova-types/src/nova-scheduling.interface.ts
+++ packages/nova-types/src/nova-scheduling.interface.ts
@@ -1,5 +1,23 @@
+/**
+ * Describes the scheduling contract UI components and their logical parent within a host application.
+ * Usage: Components should create an adapter to their scheduler of choice and provide it to the NovaSchedulingProvider.
+ * This interface out of the box is designed to work with the priorities of the Prioritized Task Scheduling API:
+ *   https://developer.mozilla.org/en-US/docs/Web/API/Prioritized_Task_Scheduling_API#task_priorities
+ * A scheduler implementation should be able to handle sending tasks to the browser's scheduler, or to a custom scheduler, and then cancelling those tasks as needed.
+ */
 export interface NovaScheduling {
+  /**
+   * Schedule a job to be run at some point in the future.
+   * @param job The job to be run.
+   * @param meta The metadata associated with the job.
+   * @returns A job identifier which can be used to cancel the job.
+   */
   schedule(job: IJob<any>, meta: IJobMetaData): number;
+  /**
+   * Cancel a job which has been scheduled.
+   * @param jobId The identifier of the job to be cancelled.
+   * @returns void
+   */
   cancel(jobId: number): void;
 }
 
@@ -12,11 +30,11 @@ export interface IJobMetaData {
   priority: JobPriority;
   /**
    * The identifier of the type of job. Note that this is not globally
-   * unique amongst all instance of a job type.
+   * unique amongst all instances of a job type.
    */
   id: string;
   /**
-   * The identifier of the type of code which scheduled the job. Not that this is
+   * The identifier of the type of code which scheduled the job. Note that this is
    * not globally unique amongst all instances of calling code.
    */
   scheduledById: string;
