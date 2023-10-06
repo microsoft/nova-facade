ccb579233020880e30c3323614956edcd3dc9e42
Drew Atkinson <dratkins@microsoft.com>
2023-10-05 17:15:16 -0700

    add useNovaUnmountEventing

--- packages/nova-react/src/eventing/nova-eventing-provider.tsx
+++ packages/nova-react/src/eventing/nova-eventing-provider.tsx
@@ -4,10 +4,16 @@ import { InputType } from "@nova/types";
 import invariant from "invariant";
 
 // Initializing default with null to make sure providers are correctly placed in the tree
-const NovaEventingContext = React.createContext<NovaReactEventing | null>(null);
+const NovaEventingContext = React.createContext<INovaEventingContext>({});
+
+interface INovaEventingContext {
+  eventing?: NovaReactEventing;
+  unmountEventing?: NovaReactEventing;
+}
 
 interface NovaEventingProviderProps {
   eventing: NovaEventing;
+  unmountEventing?: NovaEventing; // Optional version of eventing to use when unmounting, defaults to eventing if not supplied
   /**
    * Mapping logic to transform a React SyntheticEvent into a Nova
    * Source object. Supply the Nova default implemenation using the import
@@ -47,57 +53,86 @@ export interface NovaReactEventing {
   generateEvent(eventWrapper: GeneratedEventWrapper): Promise<void>;
 }
 
-export const NovaEventingProvider: React.FunctionComponent<NovaEventingProviderProps> =
-  ({ children, eventing, reactEventMapper }) => {
-    // Nova contexts provide a facade over framework functions
-    // We don't need to trigger rerender in children when we are rerendered
-    // or when the input functions change, we just need to make sure callbacks
-    // use the right functions
-    const eventingRef = React.useRef(eventing);
-    if (eventingRef.current !== eventing) {
-      eventingRef.current = eventing;
-    }
+export const NovaEventingProvider: React.FunctionComponent<
+  NovaEventingProviderProps
+> = ({ children, eventing, unmountEventing, reactEventMapper }) => {
+  // Nova contexts provide a facade over framework functions
+  // We don't need to trigger rerender in children when we are rerendered
+  // or when the input functions change, we just need to make sure callbacks
+  // use the right functions
+  const eventingRef = React.useRef(eventing);
+  if (eventingRef.current !== eventing) {
+    eventingRef.current = eventing;
+  }
 
-    const mapperRef = React.useRef(reactEventMapper);
-    if (mapperRef.current !== reactEventMapper) {
-      mapperRef.current = reactEventMapper;
-    }
+  const unmountEventingRef = React.useRef(unmountEventing || eventing);
+  if (unmountEventingRef.current !== unmountEventing) {
+    unmountEventingRef.current = unmountEventing || eventing; // default to eventing if unmountEventing not supplied
+  }
 
-    const reactEventing: NovaReactEventing = React.useMemo(
-      () => ({
-        bubble: (eventWrapper: ReactEventWrapper) => {
-          const mappedEvent: EventWrapper = mapperRef.current(eventWrapper);
-          return eventingRef.current.bubble(mappedEvent);
-        },
-        generateEvent: (eventWrapper: GeneratedEventWrapper) => {
-          const mappedEvent = {
-            event: eventWrapper.event,
-            source: {
-              inputType: InputType.programmatic,
-              timeStamp: eventWrapper.timeStampOverride ?? Date.now(),
-            },
-          };
-          return eventingRef.current.bubble(mappedEvent);
-        },
-      }),
-      [],
-    );
+  const mapperRef = React.useRef(reactEventMapper);
+  if (mapperRef.current !== reactEventMapper) {
+    mapperRef.current = reactEventMapper;
+  }
 
-    return (
-      <NovaEventingContext.Provider value={reactEventing}>
-        {children}
-      </NovaEventingContext.Provider>
-    );
-  };
+  const reactEventing: NovaReactEventing = React.useMemo(
+    generateEventing(eventingRef, mapperRef),
+    [],
+  );
+
+  const reactUnmountEventing: NovaReactEventing = React.useMemo(
+    generateEventing(unmountEventingRef, mapperRef),
+    [],
+  );
+
+  return (
+    <NovaEventingContext.Provider
+      value={{ eventing: reactEventing, unmountEventing: reactUnmountEventing }}
+    >
+      {children}
+    </NovaEventingContext.Provider>
+  );
+};
 NovaEventingProvider.displayName = "NovaEventingProvider";
 
 export const useNovaEventing = (): NovaReactEventing => {
-  const eventing = React.useContext<NovaReactEventing | null>(
-    NovaEventingContext,
-  );
+  const { eventing } = React.useContext(NovaEventingContext);
   invariant(
     eventing,
-    "Nova Eventing provider must be initialized prior to consumption!",
+    "Nova Eventing provider must be initialized prior to consumption of eventing!",
   );
   return eventing;
 };
+
+export const useNovaUnmountEventing = (): NovaReactEventing => {
+  const { unmountEventing } = React.useContext(NovaEventingContext);
+  invariant(
+    unmountEventing,
+    "Nova Eventing provider must be initialized prior to consumption of unmountEventing!",
+  );
+  return unmountEventing;
+};
+
+const generateEventing =
+  (
+    eventingRef: React.MutableRefObject<NovaEventing>,
+    mapperRef: React.MutableRefObject<
+      (reactEventWrapper: ReactEventWrapper) => EventWrapper
+    >,
+  ) =>
+  () => ({
+    bubble: (eventWrapper: ReactEventWrapper) => {
+      const mappedEvent: EventWrapper = mapperRef.current(eventWrapper);
+      return eventingRef.current.bubble(mappedEvent);
+    },
+    generateEvent: (eventWrapper: GeneratedEventWrapper) => {
+      const mappedEvent = {
+        event: eventWrapper.event,
+        source: {
+          inputType: InputType.programmatic,
+          timeStamp: eventWrapper.timeStampOverride ?? Date.now(),
+        },
+      };
+      return eventingRef.current.bubble(mappedEvent);
+    },
+  });
