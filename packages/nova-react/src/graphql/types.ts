2cb3ecea4e3f792ded18f874ea1db1b22acd58f3
Iurii Kondratiuk <iukondra@microsoft.com>
2024-03-26 10:28:11 -0700

    Use FetchPolicy in useLazyLoadQuery

--- packages/nova-react/src/graphql/types.ts
+++ packages/nova-react/src/graphql/types.ts
@@ -86,7 +86,13 @@ type FetchPolicy =
   | "store-or-network"
   | "store-and-network"
   | "network-only"
-  | "store-only";
+  | "store-only"
+  | "cache-first"
+  | "cache-only"
+  | "cache-and-network"
+  | "network-only"
+  | "no-cache"
+  | "standby";
 
 type RefetchOptions = {
   onCompleted?: (error: Error | null) => void;
