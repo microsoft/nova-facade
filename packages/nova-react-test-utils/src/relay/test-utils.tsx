2d4528b4acb8f7b596fc162821b2dff3cbaa4293
Stanislaw Wilczynski <Stanislaw.Wilczynski@microsoft.com>
2024-09-16 21:28:48 +0200

    remove wrong imports

--- packages/nova-react-test-utils/src/relay/test-utils.tsx
+++ packages/nova-react-test-utils/src/relay/test-utils.tsx
@@ -9,7 +9,6 @@ import type { OperationDescriptor as RelayOperationDescriptor } from "relay-runt
 import { novaGraphql } from "./nova-relay-graphql";
 import { createMockEnvironment } from "relay-test-utils";
 import { RelayEnvironmentProvider } from "react-relay";
-import { jest } from "@jest/globals";
 
 export class RelayMockPayloadGenerator {
   public gqlSchema: GraphQLSchema;
