7f2a3b457812f13809bcd9e3a29779faf4ae478d
Stanislaw Wilczynski <Stanislaw.Wilczynski@microsoft.com>
2024-09-19 11:05:00 +0200

    fix deps and improve comment

--- packages/examples/jest.config.ts
+++ packages/examples/jest.config.ts
@@ -4,8 +4,7 @@ export default {
   ...config,
   transform: {
     "\\.(gql|graphql)$": "@graphql-tools/jest-transform",
-    // Needed for packages/examples to load relay compiler generated artifacts.
-    // It would be better if it could be config local to examples package but haven't found a way to configure it with just
+    // Needed to load relay compiler generated artifacts.
     ".+[\\\\/]relay[\\\\/].+\\.tsx?$":
       "@graphitation/embedded-document-artefact-loader/ts-jest",
     "^.+\\.tsx?$": [
