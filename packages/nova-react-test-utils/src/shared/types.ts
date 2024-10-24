8b2cf610d40240e81d47dac7943bf9c59761d7d6
Stanislaw Wilczynski <Stanislaw.Wilczynski@microsoft.com>
2024-10-24 14:16:44 +0200

    make types for reference entries stronger and error message more friendly

--- packages/nova-react-test-utils/src/shared/types.ts
+++ packages/nova-react-test-utils/src/shared/types.ts
@@ -12,6 +12,7 @@ type OptionalArgs<T> = T extends { args?: infer A }
     : never
   : never;
 
+// Omits Z from the Storybook "args" field of T
 type OmitFromArgs<T, Z> = Omit<T, "args"> &
   Simplify<
     (Record<string, never> extends Omit<RequiredArgs<T>, keyof Z>
@@ -22,11 +23,31 @@ type OmitFromArgs<T, Z> = Omit<T, "args"> &
     { deep: true }
   >;
 
+// Check if keys of T are a subset of keys of P. Returns true if they are or union of keys that are not.
+type AreKeysSubset<T extends object, P extends object> = {
+  [K in keyof T]: K extends keyof P ? never : K;
+}[keyof T] extends never
+  ? true
+  : {
+      [K in keyof T]: K extends keyof P ? never : K;
+    }[keyof T];
+
+export type ParametersError =
+  "❌ Error: The type passed to StoryObjWithoutFragmentRefs needs to have a parameters field with novaEnvironment with referenceEntries field";
+
+export type ComponentTypeError =
+  "❌ Error: The type passed to StoryObjWithoutFragmentRefs needs to have a component field that is a React component";
+
+export type ReferenceEntriesError<K> = `❌ Error: The reference entry '${K &
+  string}' is not a property on the component's props`;
+
 export type StoryObjWithoutFragmentRefs<T> = T extends {
-  component: infer C;
-  parameters: { novaEnvironment: { referenceEntries: infer D } };
+  component?: infer C;
+  parameters: { novaEnvironment: { referenceEntries: infer D extends object } };
 }
-  ? C extends React.ComponentType<any>
-    ? OmitFromArgs<StoryObj<T>, D>
-    : never
-  : never;
+  ? C extends React.ComponentType<infer P extends object>
+    ? AreKeysSubset<D, P> extends true
+      ? OmitFromArgs<StoryObj<T>, D>
+      : ReferenceEntriesError<AreKeysSubset<D, P>>
+    : ComponentTypeError
+  : ParametersError;
