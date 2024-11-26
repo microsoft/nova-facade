fb5cca09707836e89b87ca74b6699b2b6a30a3cd
Stanislaw Wilczynski <Stanislaw.Wilczynski@microsoft.com>
2024-11-26 14:38:43 +0100

    clean up deps and migrate to v8

--- packages/examples/.storybook/main.ts
+++ packages/examples/.storybook/main.ts
@@ -1,14 +1,16 @@
+import { dirname, join } from "path";
 import type { StorybookConfig } from "@storybook/react-webpack5";
 
 const config: StorybookConfig = {
   stories: ["../src/**/*.stories.@(ts|tsx)"],
   addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
+    getAbsolutePath("@storybook/addon-links"),
+    getAbsolutePath("@storybook/addon-essentials"),
+    getAbsolutePath("@storybook/addon-interactions"),
+    getAbsolutePath("@storybook/addon-webpack5-compiler-swc")
   ],
   framework: {
-    name: "@storybook/react-webpack5",
+    name: getAbsolutePath("@storybook/react-webpack5"),
     options: {
       strictMode: true,
     },
@@ -26,19 +28,6 @@ const config: StorybookConfig = {
             loader: "@graphitation/embedded-document-artefact-loader/webpack",
           },
           {
-            test: /\.(tsx|ts)$/,
-            use: [
-              {
-                loader: require.resolve("esbuild-loader"),
-                options: {
-                  loader: "tsx",
-                  target: "es2020",
-                },
-              },
-            ],
-            exclude: /node_modules/,
-          },
-          {
             test: /\.(graphql)$/,
             loader: "@graphql-tools/webpack-loader",
           },
@@ -48,3 +37,7 @@ const config: StorybookConfig = {
   },
 };
 export default config;
+
+function getAbsolutePath(value: string): string {
+  return dirname(require.resolve(join(value, "package.json")));
+}
