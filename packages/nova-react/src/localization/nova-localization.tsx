3a34d5fc5ecd1dbec6c993f21512be1c5e34fa07
Mark Polak <mark@thedutchies.com>
2025-01-24 20:02:11 +0100

    Add localization support

---
+++ packages/nova-react/src/localization/nova-localization.tsx
@@ -0,0 +1,38 @@
+import * as React from "react";
+import type { NovaLocalization, FormatFn } from "@nova/types";
+import invariant from "invariant";
+
+const NovaLocalizationContext = React.createContext<NovaLocalization | null>(
+  null,
+);
+
+type NovaLocalizationProviderProps = {
+  localization: NovaLocalization;
+  children?: React.ReactNode | undefined;
+};
+
+export const NovaLocalizationProvider: React.FunctionComponent<
+  NovaLocalizationProviderProps
+> = ({ children, localization }) => {
+  return (
+    <NovaLocalizationContext.Provider value={localization}>
+      {children}
+    </NovaLocalizationContext.Provider>
+  );
+};
+NovaLocalizationProvider.displayName = "NovaLocalizationProvider";
+
+const useLocalization = (): NovaLocalization => {
+  const localization = React.useContext(NovaLocalizationContext);
+
+  invariant(
+    localization !== null,
+    "Nova Localization provider must be initialized prior to consumption!",
+  );
+
+  return localization;
+};
+
+export function useFormat(): FormatFn {
+  return useLocalization().useFormat();
+}
