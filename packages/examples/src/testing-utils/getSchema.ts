bd9c88052888938f723bc5d942ba310e85c1fbc5
Eloy Durán <eloy.de.enige@gmail.com>
2025-01-28 22:46:46 -0800

    [examples] Fix relay-compiler build step (#139)
    * [examples] Fix relay-compiler build step
    
    Extensions needs to be a dir, not a file
    
    * Change files

--- packages/examples/src/testing-utils/getSchema.ts
+++ packages/examples/src/testing-utils/getSchema.ts
@@ -1,4 +1,5 @@
 import { buildASTSchema } from "graphql";
 
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-export const getSchema = () => buildASTSchema(require("../schema.graphql"));
+export const getSchema = () =>
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  buildASTSchema(require("../../data/schema.graphql"));
