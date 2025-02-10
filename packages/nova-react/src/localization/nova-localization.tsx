10e90b50ef6d1311881a7f7d1622dceb59feba4a
Mark Polak <mark@thedutchies.com>
2025-02-10 09:31:55 +0100

    Add jsdoc

--- packages/nova-react/src/localization/nova-localization.tsx
+++ packages/nova-react/src/localization/nova-localization.tsx
@@ -14,6 +14,31 @@ type NovaLocalizationProviderProps = React.PropsWithChildren<{
   localization: NovaLocalization;
 }>;
 
+/**
+ * The `NovaLocalizationProvider` component is used to provide the `NovaLocalization` object to the components in your application.
+ *
+ * You should wrap your application with this component to make the `useFormat` hook available to the components.
+ *
+ * @example
+ * ```tsx
+ * import { NovaLocalization } from "@nova/types";
+ * import { NovaLocalizationProvider } from "@nova/react";
+ *
+ * const myLocalization: NovaLocalization = {
+ *   useFormat: () => (localizedStringFromGraphQL, placeholders) => {
+ *     // Your implementation here
+ *   },
+ * };
+ *
+ * const App = () => {
+ *   return (
+ *     <NovaLocalizationProvider localization={myLocalization}>
+ *       <MyComponent />
+ *     </NovaLocalizationProvider>
+ *   );
+ * };
+ * ```
+ */
 export const NovaLocalizationProvider: React.FunctionComponent<
   NovaLocalizationProviderProps
 > = ({ children, localization }) => {
@@ -52,6 +77,31 @@ function format(localizedString: unknown, values: unknown): string {
   );
 }
 
+/**
+ * Hook to access the `format` function that can be used to replace placeholders in a localized string.
+ *
+ * This hook reads the `NovaLocalization` from the context. It calls `useFormat` from the `NovaLocalization` object.
+ * If the `NovaLocalization` is not provided, it will throw an error.
+ *
+ * @returns The `format` function that can be used to replace placeholders in a localized string.
+ *
+ * @example
+ * ```tsx
+ * import { useFormat, useFragment } from "@nova/react";
+ *
+ * const MyComponent = () => {
+ *   const format = useFormat();
+ *   const viewData = useFragment(graphql`
+ *     fragment MyComponent_viewData on ViewData {
+ *       greetingLabel
+ *     }`,
+ *     props.viewData
+ *   );
+ *
+ *   return <div>{format(viewData.greetingLabel, { name: "John" })}</div>;
+ * };
+ * ```
+ */
 export function useFormat(): typeof format {
   return useLocalization().useFormat();
 }
--- packages/nova-types/src/nova-localization.ts
+++ packages/nova-types/src/nova-localization.ts
@@ -1,10 +1,32 @@
 const LocalizedString = Symbol("LocalizedString");
 
+/**
+ * Represents a localized string with placeholders.
+ * This type will be used by the `relay-compiler` when generating types for
+ * a string with placeholders.
+ *
+ * You should **not** have to use this type directly, instead use the `format` function
+ * from the `useFormat` hook to unwrap the localized string and provide the placeholders.
+ *
+ * @param T The type of the placeholders object.
+ * @example
+ * ```ts
+ * // Note: This would be part of a generated type, you should not have to write this yourself.
+ * type MyLocalizedStrings = StringWithPlaceholders<{
+ *  name: string;
+ *  age: number;
+ * }>;
+ * ```
+ */
 export type StringWithPlaceholders<T extends Record<string, string | number>> =
   {
     [LocalizedString]: T;
   };
 
+/**
+ * Extract the placeholders from a localized string.
+ * Guarantees that the placeholder object is not empty.
+ */
 export type Placeholders<T> = T extends StringWithPlaceholders<infer P>
   ? keyof P extends never // Make sure that the placeholder object is not empty
     ? never
@@ -12,18 +34,20 @@ export type Placeholders<T> = T extends StringWithPlaceholders<infer P>
   : never;
 
 type UseFormat = () => (
-  value: unknown,
-  values: Record<string, string | number>,
+  localizedStringFromGraphQL: unknown,
+  placeholders: Record<string, string | number>,
 ) => string;
 
+/**
+ * The NovaLocalization type is used to provide the `useFormat` hook to the components.
+ * The `useFormat` hook should support replacing placeholders in a localized string.
+ *
+ * You provide this type to the `NovaLocalizationProvider` component to make
+ * it available to the components in your application.
+ *
+ * The components will be able to use `useFormat` from `@nova/react` to
+ * access the `format` function.
+ */
 export type NovaLocalization = {
   useFormat: UseFormat;
 };
-
-export type FormatFn = <
-  T extends StringWithPlaceholders<P>,
-  P extends Record<string, string | number>,
->(
-  localizedString: T,
-  values: Placeholders<T>,
-) => string;
