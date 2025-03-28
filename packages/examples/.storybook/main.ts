61cdb9a7d4eac70cb6053022760c69b80237c886
Stanisław <sjwilczynski@gmail.com>
2025-03-28 23:15:27 +0100

    fix storybook running

--- packages/examples/.storybook/main.ts
+++ packages/examples/.storybook/main.ts
@@ -10,9 +10,7 @@ const config: StorybookConfig = {
   addons: [
     getAbsolutePath("@storybook/addon-links"),
     getAbsolutePath("@storybook/addon-essentials"),
-    getAbsolutePath("@storybook/addon-interactions"),
     getAbsolutePath("@storybook/experimental-addon-test"),
-    getAbsolutePath("@storybook/experimental-addon-test")
   ],
   framework: {
     name: getAbsolutePath("@storybook/react-vite"),
