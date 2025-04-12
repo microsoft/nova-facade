0ec0039e8d095863c49ddeb7354f148c9f350437
Stanisław <sjwilczynski@gmail.com>
2025-04-12 20:17:39 +0200

    use swc plugin for fast refresh and removal of babel

--- packages/examples/vitest.plugins.ts
+++ packages/examples/vitest.plugins.ts
@@ -1,5 +1,21 @@
-import relayPlugin from "vite-plugin-relay";
 import graphqlLoader from "vite-plugin-graphql-loader";
 import commonjs from "vite-plugin-commonjs";
+import react from "@vitejs/plugin-react-swc";
+import type { Config } from "@swc/plugin-relay";
 
-export const plugins = [relayPlugin, graphqlLoader(), commonjs()];
+export const plugins = [
+  react({
+    plugins: [
+      [
+        "@swc/plugin-relay",
+        {
+          rootDir: "./src",
+          language: "typescript",
+          eagerEsModules: true,
+        } satisfies Config,
+      ],
+    ],
+  }),
+  graphqlLoader(),
+  commonjs(),
+];
