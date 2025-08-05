a8483042fcac3e85359bd6a49d8b9950c39d7c1e
Stanislaw Wilczynski <stwilczy@microsoft.com>
2025-08-05 13:40:50 +0200

    add more advanced example of mock resolvers

---
+++ packages/examples/src/relay/resolvers/suggested-friends-resolvers.ts
@@ -0,0 +1,78 @@
+import type { EnvironmentMockResolversContext } from "@nova/react-test-utils";
+import type { TypeMap } from "../../__generated__/schema.all.interface";
+
+/**
+ * @RelayResolver SuggestedFriend
+ */
+export function SuggestedFriend(
+  id: string,
+  _args: any,
+  context: EnvironmentMockResolversContext<TypeMap>,
+) {
+  return context.mock.resolveById(id);
+}
+
+/**
+ * @RelayResolver PersonName
+ * @weak
+ */
+export type PersonName = { firstName: string; lastName: string };
+
+/**
+ * @RelayResolver SuggestedFriend.name: PersonName
+ */
+export function name(
+  model: any,
+  _args: any,
+  context: EnvironmentMockResolversContext<TypeMap>,
+): PersonName {
+  const data = model.name;
+  if (data === null) {
+    return data;
+  }
+
+  // Return name object directly since PersonName doesn't have an ID
+  return {
+    firstName: data?.firstName ?? context.mock.resolve("String") ?? "John",
+    lastName: data?.lastName ?? context.mock.resolve("String") ?? "Doe",
+    ...data,
+  };
+}
+
+/**
+ * @RelayResolver PersonName.firstName: String
+ */
+export function firstName(
+  model: PersonName,
+  _args: any,
+  context: EnvironmentMockResolversContext<TypeMap>,
+): string {
+  return model.firstName ?? context.mock.resolve("String") ?? "John";
+}
+
+/**
+ * @RelayResolver PersonName.lastName: String
+ */
+export function lastName(
+  model: PersonName,
+  _args: any,
+  context: EnvironmentMockResolversContext<TypeMap>,
+): string {
+  return model.lastName ?? context.mock.resolve("String") ?? "Doe";
+}
+
+/**
+ * @RelayResolver Viewer.suggestedFriends: [SuggestedFriend!]
+ */
+export function suggestedFriends(
+  _args: any,
+  context: EnvironmentMockResolversContext<TypeMap>,
+) {
+  const resolverData = context.mock.resolve("Viewer")?.suggestedFriends ?? [];
+
+  for (const item of resolverData) {
+    context.mock.storeById(item.id, item);
+  }
+
+  return resolverData;
+}
